98aa145a13dc55e480def4c711199eb1b4a0c0e6
Nick Jalbert <nickjalbert@gmail.com>
2020-02-24 15:23:36 -0800

    Setup of ray + RLlib

--- corridor.py
+++ corridor.py
@@ -1,17 +1,31 @@
 import gym
 
 
-class FooEnv(gym.Env):
+class Corridor(gym.Env):
     metadata = {"render.modes": ["human"]}
+    action_space = (0, 1)
 
-    def __init__(self):
-        pass
+    def __init__(self, config=None):
+        config = config if config else {}
+        self.length = config.get("length", 10)
+        self.reset()
 
     def step(self, action):
-        pass
+        if self.done:
+            return (self.position, 0, self.done, {})
+        if action == 0 and self.position > 0:
+            self.position -= 1
+        if action == 1 and self.position < self.length:
+            self.position += 1
+        return (self.position, -1, self.done, {})
+
+    @property
+    def done(self):
+        return self.position >= self.length
 
     def reset(self):
-        pass
+        self.position = 0
+        return (self.position,)
 
     def render(self, mode="human"):
         pass
---
+++ solve.py
@@ -0,0 +1,9 @@
+import ray
+
+
+def solve():
+    ray.init()
+
+
+if __name__ == "__main__":
+    solve()
