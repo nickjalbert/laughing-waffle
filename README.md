981c6353eb2f8eaa53bd494c8cfdf3582cd71745
Nick Jalbert <nickjalbert@gmail.com>
2020-02-24 14:49:10 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # laughing-waffle
+
 Basic implementation of an OpenAI gym environment
+
+# Dev setup
+
+Developed on OS X 10.15.2 with Python 3.7.6.
+
+* `virtualenv -p /usr/local/bin/python3 laughing-waffle`
+* `source laughing-waffle/bin/activate`
---
+++ corridor.py
@@ -0,0 +1,20 @@
+import gym
+
+
+class FooEnv(gym.Env):
+    metadata = {"render.modes": ["human"]}
+
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode="human"):
+        pass
+
+    def close(self):
+        pass
